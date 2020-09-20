cf15f7a1d5e815799a3da5cb30496daeb2c656b2
Mateus Henrique <mateushenriquechaves1@gmail.com>
2020-09-20 02:26:28 -0300

    variable name changed

--- SOLID/Open-Closed Principle/FilterExample/FilterGoodExample/GoodFilter.ts
+++ SOLID/Open-Closed Principle/FilterExample/FilterGoodExample/GoodFilter.ts
@@ -3,7 +3,7 @@ import Product from "./Product";
 import { Specification } from "./@types";
 
 export default class GoodFilter {
-  filter(products: Product[], spec: Specification): Product[] {
-    return products.filter((product) => spec.isSatisfied(product));
+  filter(products: Product[], specification: Specification): Product[] {
+    return products.filter((product) => specification.isSatisfied(product));
   }
 }
